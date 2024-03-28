c396d43dc878a122c360616e1818235cb50ad1e1
RosenYin <2356838399@qq.com>
2024-03-28 14:01:38 +0800

    增加正前方相机并修改代码另其显示

--- aloha_mujoco/aloha/scripts/aloha_ctrl_test.py
+++ aloha_mujoco/aloha/scripts/aloha_ctrl_test.py
@@ -25,6 +25,7 @@ class Mujoco_Model():
         model = load_model_from_path(model_path)
         self.sim = MjSim(model)
         self.viewer = MjViewer(self.sim)
+        self.offscreen_f = mujoco_py.MjRenderContextOffscreen(self.sim, 0, quiet = True)
         self.offscreen_l = mujoco_py.MjRenderContextOffscreen(self.sim, 0, quiet = True)
         self.offscreen_r = mujoco_py.MjRenderContextOffscreen(self.sim, 0, quiet = True)
 
@@ -83,16 +84,20 @@ class Mujoco_Model():
                 self.pos_ctrl("fr_joint3",0)
                 count = 0
             # 获取窗口屏幕图像
-            self.offscreen_l.render(width=640, height=480, camera_id=0)
+            self.offscreen_f.render(width=640, height=480, camera_id=0)
+            data_f = self.offscreen_f.read_pixels(width=640, height=480, depth=False)
+            self.offscreen_l.render(width=640, height=480, camera_id=1)
             data_l = self.offscreen_l.read_pixels(width=640, height=480, depth=False)
-            self.offscreen_r.render(width=640, height=480, camera_id=1)
+            self.offscreen_r.render(width=640, height=480, camera_id=2)
             data_r = self.offscreen_r.read_pixels(width=640, height=480, depth=False)
 
             # 从渲染结果中提取相机图像
+            image_f = np.flipud(data_f)
             image_l = np.flipud(data_l)
             image_r = np.flipud(data_r)
 
             # 显示相机图像
+            cv2.imshow("Camera Image_f", image_f)
             cv2.imshow("Camera Image_l", image_l)
             cv2.imshow("Camera Image_r", image_r)
             cv2.waitKey(1)
